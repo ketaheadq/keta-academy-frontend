fb3917a7bf681484734e834fd58652f38b8a4360
Behzat Bilgin Erdem <bbilgin.erdem@gmail.com>
2025-07-18 20:52:16 +0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        // Optional: Restrict to specific paths
+      },
+    ],
+  },
+  // Other Next.js config options can go here
 };
 
 export default nextConfig;
